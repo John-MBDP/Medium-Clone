7df8da01b29772b9c2a6a6a39be68dc46fda8edf
John-MBDP <johnartuz@hotmail.com>
2022-03-28 22:20:52 -0400

    Add new dependency called Portable Text for the blog post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,17 +1,73 @@
+import { GetStaticProps } from 'next'
 import { Header } from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
 
-function Post() {
+interface Props {
+  post: Post
+}
+
+function Post({ post }: Props) {
   return (
     <main>
       <Header />
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt=""
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="text=s, font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name} </span>-
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="my-5 text-xl font-bold" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
     </main>
   )
 }
 
 export default Post
 
+//Find the pages that exists --> prepares the page
 export const getStaticPaths = async () => {
   const query = `*[_type == "post"] {
     _id,
@@ -21,11 +77,60 @@ export const getStaticPaths = async () => {
   }`
   const posts = await sanityClient.fetch(query)
 
+  /*NextJS expects an array and whereby each object inside must contain a key called params which will contain the actual path
+    Data structure will look like this:
+     [ {
+       params: {
+         slug: "post-1"
+          }
+       }
+     ] 
+  */
   const paths = posts.map((post: Post) => ({
     params: {
       slug: post.slug.current,
     },
   }))
 
+  // blocking is use if the page doesn't exist (404)
   return { paths, fallback: 'blocking' }
+}
+
+//Have to use getStaticProps with getStaticPaths.  Handles the information from getStaticPaths.  How it's supposed to handle the id/slug
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    author-> {
+    name,
+    image
+  },
+  'comments': *[
+    _type == "comment" &&
+    post._ref == ^._id &&
+    approved == true],
+    description,
+    mainImage,
+    slug,
+    body
+  }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  //If you return an object called notFound in nextJS when using fallback blocking.  Render a 404 page
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // after 60 seconds, it'll update the old cache version
+  }
 }
